8fee499b6bf8536e3d578e93e3bd94042d0a701e
Emir <125366201+emirsassan@users.noreply.github.com>
2023-08-15 13:58:34 +0300

    Update index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
 // place files you want to import through the `$lib` alias in this folder.
 export async function fetchRepo() {
-    const res = await fetch("https://raw.githubusercontent.com/emirsassan/weave-index/main/ModIndex.json")
+    const res = await fetch("https://raw.githubusercontent.com/Zircta/Weave-Mod-Index/main/Main.json")
     return res.json()
 }
